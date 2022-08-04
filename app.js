6baf8f7ea7f6fe36f1b7ffbd46bd5c507ecda230
dragon-fire-fly <ek93@hotmail.co.uk>
2022-08-04 12:49:07 +0000

    Add user choice and random number for computer

--- app.js
+++ app.js
@@ -4,3 +4,14 @@ const resultDisplay = document.getElementById('result')
 const possibleChoices = document.querySelectorAll('button')
 let userChoice
 
+possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
+    userChoice = e.target.id
+    userChoiceDisplay.innerHTML = userChoice
+    generateComputerChoice()
+}))
+
+function generateComputerChoice() {
+    const randomNumber = Math.floor(Math.random() * possibleChoices.length)
+    console.log(randomNumber)
+
+}
