9d38f54978ce8a41ffcf3abc8986dcc8f1592d3e
dragon-fire-fly <ek93@hotmail.co.uk>
2022-08-04 12:46:21 +0000

    Add basic html and corresponding ids in js file

---
+++ app.js
@@ -0,0 +1,6 @@
+const computerChoiceDisplay = document.getElementById('computer-choice')
+const userChoiceDisplay = document.getElementById('user-choice')
+const resultDisplay = document.getElementById('result')
+const possibleChoices = document.querySelectorAll('button')
+let userChoice
+
