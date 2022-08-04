e93b8163f619081d26d852a83119ff8108b4e5c6
dragon-fire-fly <ek93@hotmail.co.uk>
2022-08-04 12:58:19 +0000

    Generate computer choice and determine winner

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const userChoiceDisplay = document.getElementById('user-choice')
 const resultDisplay = document.getElementById('result')
 const possibleChoices = document.querySelectorAll('button')
 let userChoice
+let computerChoice
+let result
 
 possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
     userChoice = e.target.id
@@ -11,7 +13,40 @@ possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click
 }))
 
 function generateComputerChoice() {
-    const randomNumber = Math.floor(Math.random() * possibleChoices.length)
-    console.log(randomNumber)
+    const randomNumber = Math.floor(Math.random() * (possibleChoices.length + 1))
+    
+    if (randomNumber === 1){
+        computerChoice = 'rock'
+    }
+    if (randomNumber === 2){
+        computerChoice = 'scissors'
+    }
+    if (randomNumber === 3){
+        computerChoice = 'paper'
+    }
+    computerChoiceDisplay.innerHTML = computerChoice
+    console.log(computerChoice)
+}
 
+function getResult(){
+    if (computerChoice === userChoice){
+        result = "It's a draw!"
+    }
+    if (computerChoice === "rock" && userChoice === "paper"){
+        result = "You win!"
+    }
+    if (computerChoice === "rock" && userChoice === "scissors"){
+        result = "You lose!"
+    }
+    if (computerChoice === "paper" && userChoice === "scissors"){
+        result = "You win!"
+    }
+    if (computerChoice === "paper" && userChoice === "rock"){
+        result = "You lose!"
+    }
+    if (computerChoice === "scissors" && userChoice === "rock"){
+        result = "You win!"
+    }if (computerChoice === "scissors" && userChoice === "paper"){
+        result = "You lose!"
+    }
 }
